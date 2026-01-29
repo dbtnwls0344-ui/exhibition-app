1a591e10dab5fe1d9faba97e99a7009cda04775f
sooo <dbtnwls0344@gmail.com>
2026-01-30 00:58:28 +0900

    하단 네비, 전체 위드값 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,8 @@
-
-export default function Home () {
-    return (
-        <section>
-            <h2>Home</h2>
-            <p>홈 입니다.</p>
-        </section>
-    )
+export default function Home() {
+  return (
+    <section>
+      <h2>Home</h2>
+      <p>홈 입니다.</p>
+    </section>
+  );
 }
